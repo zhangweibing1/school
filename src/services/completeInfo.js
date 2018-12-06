19d889c56acca8ed1f4c0a86aa379af949aa6818
yzh <yangzhaohui_vip@163.com>
2018-12-06 09:13:27 +0800

    教师 学生 个人信息编辑 service

--- src/services/completeInfo.js
+++ src/services/completeInfo.js
@@ -4,25 +4,25 @@ import request from '@/utils/request';
  * 查询省市县接口---------交校
  * @param  {[string]}
  * @return {[type]}         [description]
- */
-// export function areaData(data) {
-//   return request({
-//     method: 'put',
-//     url: '/api/cityareaInfoMobile/read/allList',
-//     data,
-//   });
-// }
+
+export function areaData(data) {
+    return request({
+        method: 'put',
+        url: '/api/cityareaInfoMobile/read/allList',
+        data,
+    });
+} */
 /**
  * 查询省市县接口-----------财专
  * @param  {[string]}
  * @return {[type]}         [description]
  */
 export function areaData(data) {
-  return request({
-    method: 'put',
-    url: '/api/mobileSysCityareainfo/read/allList',
-    data,
-  });
+    return request({
+        method: 'put',
+        url: '/api/cityareaInfoMobile/read/allList',
+        data,
+    });
 }
 
 /**
@@ -31,11 +31,24 @@ export function areaData(data) {
  * @return {[type]}         [description]
  */
 export function submitData(data = {}) {
-  return request({
-    method: 'post',
-    url: '/api/sysUserMobile/write/updateUser',
-    data,
-  });
+    return request({
+        method: 'post',
+        url: '/api/sysUserMobile/write/updateUser',
+        data,
+    });
+}
+
+/**
+ * 提交
+ * @param  {[string]}
+ * @return {[type]}         [description]
+ */
+export function submitTeaData(data = {}) {
+    return request({
+        method: 'post',
+        url: '/api/sysUserMobile/write/updateUser',
+        data,
+    });
 }
 
 /**
@@ -44,11 +57,11 @@ export function submitData(data = {}) {
  * @return {[type]}         [description]
  */
 export function Dic() {
-  return request({
-    method: 'put',
-    url: '/api/dicMobile/read/dic/List',
-    data: {},
-  });
+    return request({
+        method: 'put',
+        url: '/api/dicMobile/read/dic/List',
+        data: {},
+    });
 }
 
 /**
@@ -57,9 +70,21 @@ export function Dic() {
  * @return {[type]}         [description]
  */
 export function getStuDetail(data = {}) {
-  return request({
-    method: 'put',
-    url: '/api/sysUserMobile/read/readStu',
-    data,
-  });
+    return request({
+        method: 'put',
+        url: '/api/sysUserMobile/read/readStu',
+        data,
+    });
+}
+/**
+ * 查询教师详细信息
+ * @param  {[string]} id    [当前登录人id]
+ * @return {[type]}         [description]
+ */
+export function getTeaDetail(data = {}) {
+    return request({
+        method: 'put',
+        url: '/api/sysUserMobile/read/queryDetail',
+        data,
+    });
 }
